ecf937ceba9e5ee1cdc83aa9a59f0e27a132fe57
Jerry <jjjdormann@gmail.com>
2021-02-28 13:40:32 +0100

    Added config file

--- src/main/java/de/falsedeveloping/falseparty/Commands/Party.java
+++ src/main/java/de/falsedeveloping/falseparty/Commands/Party.java
@@ -1,7 +1,10 @@
 package de.falsedeveloping.falseparty.Commands;
 
 import de.falsedeveloping.falseparty.Main;
+import de.falsedeveloping.falseparty.Misc.InstantFirework;
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,13 +13,16 @@ import org.bukkit.entity.Player;
 public class Party implements CommandExecutor {
 
   private Main plugin;
+
   public Party(Main plugin) {
     this.plugin = plugin;
   }
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    if (!(sender instanceof Player)) return true;
+    if (!(sender instanceof Player)) {
+      return true;
+    }
     Player p = (Player) sender;
     p.getInventory().addItem(plugin.getPresentItemStack().getPresent());
     p.sendMessage("Added Item");
@@ -26,5 +32,15 @@ public class Party implements CommandExecutor {
     }
     Bukkit.broadcastMessage("§4" + p.getName() + " hat eine Party gestartet!");
     return true;
+  }
+
+  public FireworkEffect getEffect() {
+    return FireworkEffect.builder()
+        .flicker(false)
+        .trail(true)
+        .with(FireworkEffect.Type.BALL)
+        .withColor(Color.ORANGE)
+        .withFade(Color.RED)
+        .build();
   }
 }
--- src/main/java/de/falsedeveloping/falseparty/Main.java
+++ src/main/java/de/falsedeveloping/falseparty/Main.java
@@ -2,18 +2,32 @@ package de.falsedeveloping.falseparty;
 
 import de.falsedeveloping.falseparty.Commands.Party;
 import de.falsedeveloping.falseparty.Misc.PresentItemStack;
+import java.io.File;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
   private PresentItemStack presentItemStack;
   private Party party;
+  private FileConfiguration config;
 
   @Override
   public void onEnable() {
-      getCommand("partyy").setExecutor(new Party(this));
+    getCommand("partyy").setExecutor(new Party(this));
 
-      presentItemStack = new PresentItemStack(this);
+    File configFile = new File(getDataFolder(), "config.yml");
+    if (!configFile.exists()) {
+      configFile.getParentFile().mkdirs();
+      saveResource("config.yml", false);
+    }
+    presentItemStack = new PresentItemStack(this);
+    config = YamlConfiguration.loadConfiguration(configFile);
+  }
+
+  public FileConfiguration getConfiguration() {
+    return config;
   }
 
   public PresentItemStack getPresentItemStack() {
